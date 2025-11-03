9944d61102c301616d57aa66ce2e0c690005bead
mike <miguelenokjuarezochoa@gmail.com>
2025-11-03 18:24:55 -0500

    Proyecto creado. Dependencias y paqueterias necesarias instaladas

---
+++ dwv-react/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function App() {
+  
+  return (
+    <div className="App" style={{ fontFamily: 'Arial, sans-serif' }}>
+      <header style={{ padding: '15px', background: '#333', color: 'white' }}>
+        <h1>Visor DICOM - REACT </h1>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
